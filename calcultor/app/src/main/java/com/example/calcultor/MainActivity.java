e727169167e39dfc1f8bc85d01f50b2d334acba7
Dhanesh Pawar <dhaneshpawar.pro@gmail.com>
2020-02-05 09:39:36 +0530

    Calcultor app
    calculator app design completed , slipt also working

--- calcultor/app/src/main/java/com/example/calcultor/MainActivity.java
+++ calcultor/app/src/main/java/com/example/calcultor/MainActivity.java
@@ -11,8 +11,8 @@ public class MainActivity extends AppCompatActivity {
 
     Button button0, button1, button2, button3, button4, button5, button6,
             button7, button8, button9, buttonAdd, buttonSub, buttonDivision,
-            buttonMul, buttonC, buttonEqual;
-    TextView crunchifyEditText;
+            buttonMul, buttonC, buttonEqual, buttonDot;
+    TextView crunchifyEditText, myanswer;
 
     float mValueOne, mValueTwo;
 
@@ -35,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         button7 = (Button) findViewById(R.id.button7);
         button8 = (Button) findViewById(R.id.button8);
         button9 = (Button) findViewById(R.id.button9);
+        buttonDot = (Button) findViewById(R.id.buttonDot);
 
         buttonAdd = (Button) findViewById(R.id.buttonPlus);
         buttonSub = (Button) findViewById(R.id.buttonSubtract);
@@ -44,68 +45,145 @@ public class MainActivity extends AppCompatActivity {
         buttonEqual = (Button) findViewById(R.id.buttonequals);
 
         crunchifyEditText = (TextView) findViewById(R.id.mytextview);
+        myanswer = (TextView) findViewById(R.id.myanswer);
+
+
+        crunchifyEditText.setText("0");
+        myanswer.setText("0");
+
+        buttonDot.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if (crunchifyEditText.getText() == "0") {
+                    crunchifyEditText.setText(".");
+                }
+                else
+                {
+                    crunchifyEditText.setText(crunchifyEditText.getText() + ".");
+                }
+            }
+        });
 
         button0.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                crunchifyEditText.setText(crunchifyEditText.getText() + "0");
+
+                if (crunchifyEditText.getText() == "0") {
+                    crunchifyEditText.setText("0");
+                }
+                else
+                {
+                    crunchifyEditText.setText(crunchifyEditText.getText() + "0");
+                }
             }
         });
 
         button1.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                crunchifyEditText.setText(crunchifyEditText.getText() + "1");
+                if (crunchifyEditText.getText() == "0") {
+                    crunchifyEditText.setText("1");
+                }
+                else {
+                    crunchifyEditText.setText(crunchifyEditText.getText() + "1");
+                }
             }
         });
 
         button2.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                crunchifyEditText.setText(crunchifyEditText.getText() + "2");
+                if (crunchifyEditText.getText() == "0") {
+                    crunchifyEditText.setText("2");
+                }
+                else {
+                    crunchifyEditText.setText(crunchifyEditText.getText() + "2");
+                }
             }
         });
 
         button3.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                crunchifyEditText.setText(crunchifyEditText.getText() + "3");
+                if (crunchifyEditText.getText() == "0") {
+                    crunchifyEditText.setText("3");
+                }
+                else {
+
+                    crunchifyEditText.setText(crunchifyEditText.getText() + "3");
+                }
             }
         });
         button4.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                crunchifyEditText.setText(crunchifyEditText.getText() + "4");
+                if (crunchifyEditText.getText() == "0") {
+                    crunchifyEditText.setText("4");
+                }
+                else {
+
+                    crunchifyEditText.setText(crunchifyEditText.getText() + "4");
+                }
             }
         });
         button5.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                crunchifyEditText.setText(crunchifyEditText.getText() + "5");
+                if (crunchifyEditText.getText() == "0") {
+                    crunchifyEditText.setText("5");
+                }
+                else {
+
+                    crunchifyEditText.setText(crunchifyEditText.getText() + "5");
+                }
             }
         });
         button6.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                crunchifyEditText.setText(crunchifyEditText.getText() + "6");
+                if (crunchifyEditText.getText() == "0") {
+                    crunchifyEditText.setText("6");
+                }
+                else {
+
+                    crunchifyEditText.setText(crunchifyEditText.getText() + "6");
+                }
             }
         });
         button7.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                crunchifyEditText.setText(crunchifyEditText.getText() + "7");
+                if (crunchifyEditText.getText() == "0") {
+                    crunchifyEditText.setText("7");
+                }
+                else {
+
+                    crunchifyEditText.setText(crunchifyEditText.getText() + "7");
+                }
             }
         });
         button8.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                crunchifyEditText.setText(crunchifyEditText.getText() + "8");
+                if (crunchifyEditText.getText() == "0") {
+                    crunchifyEditText.setText("8");
+                }
+                else {
+
+                    crunchifyEditText.setText(crunchifyEditText.getText() + "8");
+                }
             }
         });
         button9.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                crunchifyEditText.setText(crunchifyEditText.getText() + "9");
+                if (crunchifyEditText.getText() == "0") {
+                    crunchifyEditText.setText("9");
+                }
+                else {
+
+                    crunchifyEditText.setText(crunchifyEditText.getText() + "9");
+                }
             }
         });
 
@@ -115,12 +193,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                if (crunchifyEditText == null) {
-                    crunchifyEditText.setText("");
-                } else {
+                if (crunchifyEditText.getText() == "0") {
+                    crunchifyEditText.setText("0");
+                }
+                else if(crunchifyAddition == false)
+                {
                     mValueOne = Float.parseFloat(crunchifyEditText.getText() + "");
                     crunchifyAddition = true;
-                    crunchifyEditText.setText(null);
+                    crunchifyEditText.setText(crunchifyEditText.getText() + " + ");
+                }
+                else
+                {
+
                 }
             }
         });
@@ -128,42 +212,63 @@ public class MainActivity extends AppCompatActivity {
         buttonSub.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mValueOne = Float.parseFloat(crunchifyEditText.getText() + "");
-                mSubtract = true;
-                crunchifyEditText.setText(null);
+
+                if (crunchifyEditText.getText() == "0") {
+                    crunchifyEditText.setText("-");
+                } else if(mSubtract == false) {
+                    mValueOne = Float.parseFloat(crunchifyEditText.getText() + "");
+                    mSubtract = true;
+                    crunchifyEditText.setText(crunchifyEditText.getText() + " - ");
+                }
             }
         });
 
         buttonMul.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mValueOne = Float.parseFloat(crunchifyEditText.getText() + "");
+                if (crunchifyEditText.getText() == "0") {
+                    crunchifyEditText.setText("0");
+                } else {
+
+                    mValueOne = Float.parseFloat(crunchifyEditText.getText() + "");
                 crunchifyMultiplication = true;
-                crunchifyEditText.setText(null);
+                crunchifyEditText.setText(null);}
             }
         });
 
         buttonDivision.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mValueOne = Float.parseFloat(crunchifyEditText.getText() + "");
-                crunchifyDivision = true;
-                crunchifyEditText.setText(null);
-            }
+                if (crunchifyEditText.getText() == "0") {
+                    crunchifyEditText.setText("0");
+                } else {
+                    mValueOne = Float.parseFloat(crunchifyEditText.getText() + "");
+                    crunchifyDivision = true;
+                    crunchifyEditText.setText(null);
+                }}
         });
 
         buttonEqual.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mValueTwo = Float.parseFloat(crunchifyEditText.getText() + "");
+
+//                mValueTwo = Float.parseFloat(crunchifyEditText.getText() + "");
 
                 if (crunchifyAddition == true) {
-                    crunchifyEditText.setText(mValueOne + mValueTwo + "");
+                    String mystr = crunchifyEditText.getText().toString();
+                    String[] myvals = mystr.split(" ");
+                    int addtiond = Integer.parseInt(myvals[0])+Integer.parseInt(myvals[2]);
+                    crunchifyEditText.setText(""+addtiond);
+                    myanswer.setText(""+addtiond);
                     crunchifyAddition = false;
                 }
 
                 if (mSubtract == true) {
-                    crunchifyEditText.setText(mValueOne - mValueTwo + "");
+                    String mystr = crunchifyEditText.getText().toString();
+                    String[] myvals = mystr.split(" ");
+                    int addtiond = Integer.parseInt(myvals[0])-Integer.parseInt(myvals[2]);
+                    crunchifyEditText.setText(""+addtiond);
+                    myanswer.setText(""+addtiond);
                     mSubtract = false;
                 }
 
@@ -182,7 +287,8 @@ public class MainActivity extends AppCompatActivity {
         buttonC.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                crunchifyEditText.setText("");
+                crunchifyEditText.setText("0");
+                myanswer.setText("0");
             }
         });
 
